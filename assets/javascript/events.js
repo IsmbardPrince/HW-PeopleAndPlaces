7511c5419bc5d20e19934a44f386e3a71a305d93
Will Palafox <wpalafox@gmail.com>
2016-11-21 22:25:45 -0600

    HTML update and added my functions and onclick events.

--- assets/javascript/events.js
+++ assets/javascript/events.js
@@ -17,4 +17,66 @@ function handleGapiScriptLoad() {
 
 	gapiScriptLoaded = true;
 
+
+
+/////////////////////ON CLICK EVENTS///////////////////////////////////////////////////
+
+//This function adds user inputed labels to the labelArray
+
+$("#addLabelButton").on("click", function(){
+
+	//Captures the user input value and puts into a variable
+	var label = $('#labelBox').val().trim();
+
+	//Pushes the variable into an array
+	labelsArray.push(label);
+
+	//Console logs the array for development purposes
+	console.log(labelsArray);
+	
+	//Calls the render labels function
+	renderLabels();
+	
+	//Prevents the form from discarding the console.log 
+	event.preventDefault();
+
+});
+
+//This on click event calls the editContact function
+
+$(".editButton").on("click", editContact);
+
+//This on click event calls the deleteLabelButton function
+
+
+$("#deleteLabelButton").on("click", deleteLabel);
+
+
+//This on click event resets all of the label displays 
+
+$('#resetLabelsButton').on('click', function(){
+
+
+
+	$('#labelsDisplay').empty();	
+	$('#restaurantDisplay').empty();
+	$('#activitiesDisplay').empty();
+	$('#nightDisplay').empty();
+
+
+});
+
+
+
+
+
+///////////////////////////////////////////////////////////
+
+
+
+
+
+
+
 }
+
